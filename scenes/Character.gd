ea12c1394a53b30f4ed17441064e55a2ccb2f09f
CliffsDover <cliffs@dancingbottle.com>
2018-02-08 18:00:36 +0800

    Add WalkLeft and WalkUp animations

--- scenes/Character.gd
+++ scenes/Character.gd
@@ -16,29 +16,45 @@ func _process(delta):
 	velocity = Vector2()
 	if Input.is_action_pressed( "ui_right" ):
 		velocity.x += 1
+		$AnimationPlayer.play( "WalkRight" )
 	if Input.is_action_pressed( "ui_left" ):
 		velocity.x -= 1
+		$AnimationPlayer.play( "WalkLeft" )
 	if Input.is_action_pressed( "ui_down" ):
 		velocity.y += 1
+		$AnimationPlayer.play( "WalkDown" )
 	if Input.is_action_pressed( "ui_up" ):
 		velocity.y -= 1
+		$AnimationPlayer.play( "WalkUp" )
 		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
 		#$AnimationPlayer.play( "WalkDown" )
 		#$AnimatedSprite.play()
 	#else:
-	#	$AnimatedSprite.stop()	
+	#	$AnimationPlayer.stop()
 	position += velocity * delta
 	position.x = clamp( position.x, 0, screenSize.x )
 	position.y = clamp( position.y, 0, screenSize.y )
 	
 	
-	if velocity.x != 0:
-		$AnimationPlayer.play( "WalkRight" )
+	#if velocity.x != 0:
+		#$AnimationPlayer.play( "WalkRight" )
 		#$AnimatedSprite.flip_v = false
 		#$AnimatedSprite.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimationPlayer.play( "WalkDown" )
+	#	$Body.flip_h = velocity.x < 0
+	#	$Hair.flip_h = velocity.x < 0
+	#	$Coat.flip_h = velocity.x < 0
+	#	$Trousers.flip_h = velocity.x < 0
+#		$Shoes.flip_h = velocity.x < 0
+		
+#		$Body.flip_v = false
+#		$Hair.flip_v = false
+#		$Coat.flip_v = false
+#		$Trousers.flip_v = false
+#		$Shoes.flip_v = false
+		
+#	elif velocity.y != 0:
+#		$AnimationPlayer.play( "WalkDown" )
 		#$AnimatedSprite.animation = "up"
 		#$AnimatedSprite.flip_v = velocity.y > 0
